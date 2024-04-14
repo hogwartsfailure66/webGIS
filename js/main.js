13be3edcec3197872aedd63e0f44752dad459cc8
hogwartsfailure66 <3309968337@qq.com>
2024-04-14 12:27:47 -0500

    added instr & about modals w/ js

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const confirmButton = document.querySelector("#confirm");
+const instructionButton = document.querySelector("#instruction");
+const aboutButton = document.querySelector("#about");
+const messageDiv = document.querySelector(".message-container");
+const messageContent = document.querySelector(".message");
+
+const aboutContent =
+  "<h2>Acknowledgments</h2>" +
+  "<h3>Data</h3>" +
+  "<p>data sources...</p>" +
+  "<h3>Libraries</h3>" +
+  "<ul>" +
+  "<li>ArcGis Online</li>" +
+  "<li>...</li>" +
+  "<li><a href='https://fontawesome.com/' target='_blank'>Font Awesome</a> for icons</li>" +
+  "</ul>";
+
+const instructionContent =
+  "<h2>About this project<h2>" +
+  "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium debitis corporis deleniti asperiores qui? Repellendus modi maxime explicabo architecto, placeat asperiores, sed, quae numquam deserunt magni assumenda! Commodi, maiores animi?</p>" +
+  "<h2>Project Goals</h2>" +
+  "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At cum neque sed quos, dignissimos asperiores, suscipit iste culpa cumque voluptatum ipsa obcaecati perferendis veritatis, sunt architecto modi fugit facilis blanditiis quis. Laborum labore, tempore nesciunt voluptates nihil excepturi vel vitae sunt reprehenderit, ipsa eos dolore asperiores nostrum aspernatur alias placeat deleniti suscipit illum ut, consectetur veritatis provident debitis? Labore, ratione nihil, suscipit, eum ipsum delectus atque nulla commodi fuga adipisci harum explicabo. Mollitia enim similique voluptate tempora, iusto perspiciatis assumenda molestias! Assumenda itaque cum veritatis, delectus quibusdam provident facilis inventore magni soluta aut fuga rerum, harum eum debitis placeat sed?Lorem ipsum dolor, sit amet consectetur adipisicing elit. At cum neque sed quos, dignissimos asperiores, suscipit iste culpa cumque voluptatum ipsa obcaecati perferendis veritatis, sunt architecto modi fugit facilis blanditiis quis. Laborum labore, tempore nesciunt voluptates nihil excepturi vel vitae sunt reprehenderit, ipsa eos dolore asperiores nostrum aspernatur alias placeat deleniti suscipit illum ut, consectetur veritatis provident debitis? Labore, ratione nihil, suscipit, eum ipsum delectus atque nulla commodi fuga adipisci harum explicabo. Mollitia enim similique voluptate tempora, iusto perspiciatis assumenda molestias! Assumenda itaque cum veritatis, delectus quibusdam provident facilis inventore magni soluta aut fuga rerum, harum eum debitis placeat sed?</p>";
+
+const init = () => {
+  confirmButton.addEventListener("click", closeMessageDiv);
+  instructionButton.addEventListener("click", showInstruction);
+  aboutButton.addEventListener("click", showAbout);
+  messageDiv.addEventListener("click", closeMessageDiv);
+};
+
+const closeMessageDiv = () => {
+  messageDiv.classList.remove("show");
+};
+
+const showInstruction = () => {
+  showMessageDiv(instructionContent);
+};
+
+const showAbout = () => {
+  console.log("about");
+  showMessageDiv(aboutContent);
+};
+
+const showMessageDiv = (message) => {
+  messageContent.innerHTML = message;
+  messageDiv.classList.add("show");
+};
+
+init();
