d2758907072e67de949b6f80b8d3e3a576c3f933
hogwartsfailure66 <3309968337@qq.com>
2024-04-21 13:45:06 -0500

    changed search result marker color to green

--- js/main.js
+++ js/main.js
@@ -107,9 +107,20 @@ const searchControl = L.esri.Geocoding.geosearch({
 
 const results = L.layerGroup().addTo(map);
 
+var greenIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 searchControl.on("results", function (data) {
   results.clearLayers();
   for (let i = data.results.length - 1; i >= 0; i--) {
-    results.addLayer(L.marker(data.results[i].latlng));
+    results.addLayer(L.marker(data.results[i].latlng, { icon: greenIcon }));
   }
 });
