032f8e6d658846bf1572ed26f7469b8423014f0c
hogwartsfailure66 <3309968337@qq.com>
2024-04-21 13:52:15 -0500

    changed green marker src

--- js/main.js
+++ js/main.js
@@ -108,10 +108,8 @@ const searchControl = L.esri.Geocoding.geosearch({
 const results = L.layerGroup().addTo(map);
 
 var greenIcon = new L.Icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconUrl: "img/marker-icon-2x-green.png",
+  shadowUrl: "img/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
