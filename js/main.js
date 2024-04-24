70b45dfefb3a08b8738e49895316c3ab3dca70d3
hogwartsfailure66 <3309968337@qq.com>
2024-04-23 19:58:41 -0500

    changed legend

--- js/main.js
+++ js/main.js
@@ -344,13 +344,8 @@ const legend = L.control
       },
       {
         label: " must cross a major road",
-        type: "rectangle",
-        fillColor: "#555555",
-        color: "#555555",
-        opacity: 0.8,
-        fillOpacity: 0.5,
-        fillPattern: stripes,
-        weight: 2,
+        type: "image",
+        url: "img/legend_major_rd.png",
       },
     ],
   })
